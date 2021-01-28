f659b36c57af8980f3f89c856e931fb926112f88
Raghava-Kanike <iraghavakanike@gmail.com>
2021-01-28 19:55:08 +0530

    revstr v2

--- Competitive Programming/Strings/ReverseString.java
+++ Competitive Programming/Strings/ReverseString.java
@@ -15,12 +15,31 @@ public class ReverseString {
 		return revStr;
 		
 	}
+	public void reverseArraystr(String str) {
+		 char arr[]=str.toCharArray();	
+		
+			if(arr.length<=1 || arr==null) {
+				System.out.println("enter a valid array");
+			}
+			for(int i=0;i<arr.length/2;i++)
+			{
+						
+				char temp=arr[i];
+				arr[i]=arr[arr.length-1-i];
+				arr[arr.length-1-i]=temp; 
+				
+			}		
+			
+			System.out.println(new String(arr));
+			
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str="I Love Java";
 		ReverseString revstr=new ReverseString();
 		System.out.println(revstr.reverseStr(str));
-
+		revstr.reverseArraystr(str);
+		
 	}
 
 }
